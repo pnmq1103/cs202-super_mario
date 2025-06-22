718a613362ae3b0ed8417e3d8c43471357c32c9b
bao2434 <ncbao2434@apcs.fitus.edu.vn>
2025-06-22 11:41:23 +0700

    change the machanism of moving and jumping

---
+++ include/Character.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include <raylib.h>
+#include <vector>
+enum CharacterType{
+    MARIO,
+    LUIGI
+};
+class CharacterState;
+class Character{
+    private:
+        CharacterState *pState;
+        CharacterType type;
+        int state;
+        float scale;
+    public:
+        Character(float Nscale);
+        ~Character();
+        void setCharacter(CharacterType type,Vector2 pos);
+        void setState(int state);
+        void draw();
+        void run(bool left);
+        void jump();
+        void update();
+        void setFrameCount();
+};
+
+
