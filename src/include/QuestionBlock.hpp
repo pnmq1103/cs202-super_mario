a0bebbf214bb1582045375ea0c4ff2556143a70d
ntqduy2908 <56163606+ntqduy2908@users.noreply.github.com>
2025-07-03 16:22:39 +0700

    add class Block and QuestionBlock

---
+++ src/include/QuestionBlock.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include "Block.hpp"
+
+enum class PowerUpType {
+    None,
+    Coin,
+    SuperMushroom,
+    FireFlower,
+    Star
+};
+
+class QuestionBlock : public Block {
+private:
+    int coinCount = 0;
+    int maxCoins = 10;
+    bool used = false;
+    PowerUpType item;
+
+public:
+    QuestionBlock(Vector2 pos, PowerUpType item = PowerUpType::Coin, int spriteId = 0);
+
+    void onHit() override;
+    void update(float dt) override;
+
+    bool isUsed() const;
+    PowerUpType getItem() const;
+};
