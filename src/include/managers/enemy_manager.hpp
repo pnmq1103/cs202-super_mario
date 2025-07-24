8309c4566d45a993cdb81f8f0d1cb4580cc97691
ntqduy2908 <56163606+ntqduy2908@users.noreply.github.com>
2025-07-24 17:42:32 +0700

    add manager for block, item, enemy

---
+++ src/include/managers/enemy_manager.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include "enemies/enemy.hpp"
+#include <vector>
+#include <memory>
+
+class EnemyManager {
+private:
+  std::vector<Enemy*> enemies;
+
+public:
+  ~EnemyManager();
+
+  void AddEnemy(Enemy* enemy);
+  void UpdateAll(float dt);
+  void RenderAll(Texture &tex, const std::unordered_map<int, Rectangle> &spriteRects);
+  void ClearDeadEnemies(); 
+
+  std::vector<Enemy*>& GetEnemies(); 
+};
