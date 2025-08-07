91be95555af88ad2c767c75362deb8cc758fcbed
pnmq <minhquan8046@gmail.com>
2025-08-07 14:52:07 +0700

    Fix map1
    - The error is due to the coin being drawn in wrong layer (Tile2 which is only for pipe instead of Tile1)

--- src/core/game.cpp
+++ src/core/game.cpp
@@ -60,9 +60,9 @@ void GameScene::Init() {
   game_manager_.RegisterCharacterWithCollision(player_character_);
   ObjectManager::GetInstance().Reset(
     (int)constants::scale, &collision_handler_);
-  // game_manager_.LoadLevel("res/maps/map1.json");
-  game_manager_.LoadLevel("res/saves/map_00.json");
-  // CreateSimpleTestLevel();
+  game_manager_.LoadLevel("res/maps/map1.json");
+  // game_manager_.LoadLevel("res/saves/map_00.json");
+  //  CreateSimpleTestLevel();
   player_character_->SetCharacter(character_type_, {10.0f, 500.0f});
 }
 
